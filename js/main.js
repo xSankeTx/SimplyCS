f003370a91f752dfbfd8ac44afc136ac34a97164
xSankeTx <2mhatre3@gmail.com>
2020-01-16 10:55:45 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    /*OWL CAROUSEL FOR BLOG */
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    });
+
+});
